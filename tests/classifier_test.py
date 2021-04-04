5cd4395ec9224bfdf9af0022e4bfd1c49419acdd
amandafbri <42866976+amandafbri@users.noreply.github.com>
2021-04-04 09:12:57 -0300

    Add test and test data

---
+++ tests/classifier_test.py
@@ -0,0 +1,23 @@
+import json
+
+from unittest import TestCase
+
+from xray_classifier_api import flask_api
+
+
+class FlaskAPItests(TestCase):
+    def setUp(self):
+        self.client = flask_api.app.test_client()
+
+    def test_view_position_classification(self):
+        with open('tests/TestData/request.json', 'r') as _file:
+            request = json.load(_file)
+        response = self.client.post(
+            '/predict',
+            data=json.dumps(request)
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            json.loads(response.data)['message'],
+            'This is an X-ray with L view position.'
+        )
