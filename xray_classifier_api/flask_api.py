4101aab6e4ef5fbdf119a1147a49359d665c453c
amandafbri <42866976+amandafbri@users.noreply.github.com>
2021-04-06 11:53:47 -0300

    Update port to get from env

--- xray_classifier_api/flask_api.py
+++ xray_classifier_api/flask_api.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 from flask import Flask, request, jsonify
@@ -22,4 +23,5 @@ class XrayViewPositionClassifier(Resource):
 
 
 def create_app():
-    return app.run(host='0.0.0.0', port=5005)
+    port = int(os.environ.get('PORT', 5000))
+    return app.run(host='0.0.0.0', port=port)
